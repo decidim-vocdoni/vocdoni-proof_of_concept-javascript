068bb575e8bd58180f862efc96027a13ba5174c9
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-12-12 14:28:39 +0100

    Add getAccountInfo and showAccountInfo functions

--- src/app.js
+++ src/app.js
@@ -71,12 +71,34 @@ const getWallet = async () => {
 }
 
 /* Sets up the account in the Vocdoni API. 
- * @param {Object} client
+ * @param {VocdoniSDKClient} client An instance of the client with the account created
  * @returns {void}
  */
 const setupAccount = async (client) => {
   console.log("Set up account");
   await client.createAccount();
+}
+
+/* Gets the account information from the Vocdoni API.
+ * @param {VocdoniSDKClient} client An instance of the client with the account created
+ * @returns {Promise<Object>} A promise to the account information
+ */
+const getAccountInfo = (client) => {
+  console.log("Getting account information");
+  return new Promise((resolve) => resolve(client.fetchAccountInfo()));
+}
+
+/* Show the account information in a description list (<DL>) with the "js-account-information" HTML class
+ * @param {Object} accountInformation The account information with the key and value
+ *   The object keys that we want to show must correspond with the key in the DD markup (<dd data-account="key"></dd>)
+ * @returns {void}
+ */
+const showAccountInfo = (accountInformation) => {
+  const accountInformationDescription = document.querySelector(".js-account-information");
+  accountInformationDescription.classList.toggle("hide");
+  accountInformationDescription.querySelectorAll("dd").forEach((element) => {
+    element.innerHTML = accountInformation[element.dataset["account"]];
+  })
 }
 
 /* Sets up the census. This is only for demo purposes.
@@ -161,6 +183,9 @@ const main = async () => {
 
   console.log("Creating account...");
   setupAccount(client);
+  const accountInformation = await getAccountInfo(client);
+  console.log("ACCOUNT INFO => ", accountInformation);
+  showAccountInfo(accountInformation);
 
   console.log("Let's change the buttons");
   ethereumButton.classList.toggle("hide");
