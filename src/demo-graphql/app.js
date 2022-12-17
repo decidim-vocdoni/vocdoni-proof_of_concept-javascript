baaf0b7b2ff80a9c6238220b6f99f39650828894
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-12-17 08:46:33 +0100

    Refactor the GraphQL API demo app

--- src/demo-graphql/app.js
+++ src/demo-graphql/app.js
@@ -1,13 +1,32 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const index = fs.readFileSync(path.resolve(path.dirname(require.main.filename), "election.json"));
 const port = 8081;
 
+const readDataFile = (filename) => {
+  return fs.readFileSync(path.resolve(path.dirname(require.main.filename), filename))
+}
+
 http.
-  createServer((_req, res) => {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(index);
+  createServer((req, res) => {
+    let response = "";
+
+    switch (req.url) {
+      case "/api":
+        contentType = "application/json";
+        response = readDataFile("election.json");
+        break;
+      default:
+        contentType = "text/plain";
+        response = "Unknown query";
+    }
+
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "http://localhost:8080",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Content-Type": contentType
+    });
+    res.end(response);
   }).
   listen(port);
 
