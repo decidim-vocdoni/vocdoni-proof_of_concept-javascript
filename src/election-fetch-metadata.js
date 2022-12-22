d1275992c61c3055c82232db5af717872ea5554f
Andrés Pereira de Lucena <andreslucena@gmail.com>
2022-12-22 08:34:14 +0100

    Add documentation in FetchVocdoniElectionMetadata

--- src/election-fetch-metadata.js
+++ src/election-fetch-metadata.js
@@ -61,6 +61,11 @@ export default class FetchVocdoniElectionMetadata {
     }
   }
 
+  /*
+   * Fetch the Election metadata fron the Vocdoni API
+   *
+   * @returns {Promise<object>} A promise of the Election metadata
+   */
   _fetchElection() {
     // Add a bit of delay to give time to the client to be set up
     return new Promise(resolve => {
@@ -75,11 +80,28 @@ export default class FetchVocdoniElectionMetadata {
     });
   }
 
+  /*
+   * Shows the Election metadata in the markup
+   *
+   * @param {object} electionMetadata The full response with the Election metadata from the Vocdoni API
+   *
+   * @returns {void}
+   */
   _showElectionMetadata(electionMetadata) {
     this.electionMetadataDiv.parentElement.classList.remove("hide");
     this.electionMetadataDiv.textContent = JSON.stringify(electionMetadata, null, 4);
   }
 
+  /*
+   * Update the status of the election. For demo purposes, we store it in window.localStorage.
+   * In the real app, this will be handled with the rake task `bin/rails decidim_elections:scheduled_tasks`
+   *
+   * @link {https://docs.vocdoni.io/architecture/smart-contracts/process.html#process-status}
+   *
+   * @param {object} electionMetadata The full response with the Election metadata from the Vocdoni API
+   *
+   * @returns {void}
+   */
   _updateStatus(electionMetadata) {
     const now = new Date().getTime();
     const startDate = Date.parse(electionMetadata.startDate);
